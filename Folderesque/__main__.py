9a99ac0ed49d2c7fdbdd7ac83c44d66644e70f7a
Sevilze <sevilzcubing@gmail.com>
2025-02-27 21:56:24 +0700

    add setup script and rename project folder

--- Folderesque/__main__.py
+++ Folderesque/__main__.py
@@ -1,14 +1,18 @@
-from esrgan import AnimeESRGAN
-from config import INPUT_PATH, OUTPUT_PATH, MODEL_PATH, DEVICE
-
+import argparse
+from .esrgan import AnimeESRGAN
+from .config import INPUT_PATH, OUTPUT_PATH, MODEL_PATH, DEVICE
 
 def main():
+    parser = argparse.ArgumentParser(
+        description="Upscale images using AnimeESRGAN."
+    )
+    args = parser.parse_args()
+    
     input_dir = INPUT_PATH
     output_dir = OUTPUT_PATH
     upscaler = AnimeESRGAN(output_dir, MODEL_PATH, DEVICE)
     upscaler.process_folder(input_dir=input_dir, output_dir=output_dir)
     print("Upscaling complete!")
-
 
 if __name__ == "__main__":
     main()
