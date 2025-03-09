c6b54d3b9bcaecf3fd0e6c8a435595afd887f23c
Sevilze <sevilzcubing@gmail.com>
2025-03-09 19:51:46 +0700

    Reworking the batched processing for faster initialization during the first few seconds an image is loaded.

--- Folderesque/config.py
+++ Folderesque/config.py
@@ -1,5 +1,5 @@
-INPUT_PATH = "daskruns2"
-OUTPUT_PATH = "daskruns2"
+INPUT_PATH = "daskruns"
+OUTPUT_PATH = "testscaling"
 MODEL_PATH = "models\RealESRGAN_x4plus_anime_6B.pth"
 SCALE_FACTOR = 4
 DEVICE = "cuda"
