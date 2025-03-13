204b2abe90f0949835fb209d6cb1ff552a74f767
Sevilze <sevilzcubing@gmail.com>
2025-03-13 22:37:53 +0700

    Modifying the setup script to include the config file after installation.

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
+import os
+import shutil
 import pathlib
 from setuptools import setup, find_packages
+from setuptools.command.install import install
 
 setup_path = pathlib.Path(__file__).parent
 readme = (setup_path / "README.md").read_text(encoding="utf-8")
@@ -7,9 +10,18 @@ readme = (setup_path / "README.md").read_text(encoding="utf-8")
 with (setup_path / "requirements.txt").open() as f:
     requirements = f.read().splitlines()
 
+
+class InstallConfig(install):
+    def run(self):
+        install.run(self)
+        config_path = os.path.join(os.path.dirname(__file__), "Folderesque", "config.py")
+        install_dir = os.path.join(os.path.dirname(__file__), "config.py")
+        shutil.copy(config_path, install_dir)
+
+
 setup(
     name="Folderesque",
-    version="0.1.2",
+    version="0.1.3",
     description="Python Script to process and upscale images in specified folders using RRDB models.",
     long_description=readme,
     long_description_content_type="text/markdown",
@@ -23,5 +35,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=requirements,
-    package_data={"Folderesque": ["config.py"]},
+    cmdclass={
+        "install": InstallConfig,
+    },
 )
